048a23cfa4d7c38cc678228e9de3463e8ca739b1
Alex Litvin <programer95@gmail.com>
2015-10-14 21:47:58 +0300

    init

---
+++ README.rst
@@ -0,0 +1,5 @@
+Grail SDK
+=========
+
+Grail SDK (Standart Development Kit) in python. This SDK used in original Grail application and with it you can develop compatible extensions to Grail.
+SDK includes handling of Project, CueList's, Cue and reading/writing to *.grail files.
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Grail SDK
+# Copyright (C) 2015 Oleksii Lytvyn
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='grail.sdk',
+    version='0.1',
+    author='Oleksii Lytvyn',
+    author_email='grailapplication@gmail.com',
+    description=(
+        'Grail SDK'),
+    long_description=open('README.rst').read(),
+    url='https://bitbucket.org/grailapp/sdk',
+    platforms='any',
+    packages=['grail.sdk'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: System :: Networking',
+    ],
+    install_requires=[]
+)
