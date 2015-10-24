fc7946294568e6389ad3804a832059540774be2e
Alex Litvin <programer95@gmail.com>
2015-10-24 12:14:14 +0300

    bible module fixes

--- grail/sdk/bible.py
+++ grail/sdk/bible.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import os
+import re
 import sqlite3 as lite
-from db import DataBase, DataBaseHost
+from db import DataBaseHost
 
 
 def verse_factory( cursor, row ):
@@ -24,6 +26,7 @@ class Verse:
   _osisid = ""
   
   _book = ""
+  _book_id = 1
   _chapter = 1
   _verse = 1
 
@@ -37,6 +40,10 @@ class Verse:
 
   @property
   def book( self ):
+    return self._book
+
+  @property
+  def book_id( self ):
     return self._book
 
   @property
@@ -57,11 +64,12 @@ class Verse:
     return self._text
 
   def parse( self, row ):
-    self._book = row[0]
-    self._chapter = row[1]
-    self._verse = row[2]
-    self._text = row[3]
-    self._osisid = ""
+    self._book = row[5]
+    self._book_id = row[1]
+    self._chapter = row[2]
+    self._verse = row[3]
+    self._text = row[4]
+    self._osisid = row[0]
 
 
 class Book:
@@ -104,10 +112,10 @@ class Book:
 
   def parse( self, row, cursor ):
     self._id = row[0]
-    self._abbr = row[3]
-    self._name = row[1]
-    self._title = row[2]
-    self._osisid = ""
+    self._abbr = row[4]
+    self._name = row[2]
+    self._title = row[3]
+    self._osisid = row[1]
 
 
 class Bible:
@@ -133,7 +141,7 @@ class Bible:
     """Read grail bible format into Bible class"""
     
     if not Bible.validate( file_path ):
-      raise BibleError("Bible file coud not be opened.")
+      raise BibleError("Bible file could not be opened.")
     else:
       self._db = DataBaseHost.get( file_path )
 
@@ -146,6 +154,8 @@ class Bible:
     self._copyright = self._get_property("copyright", "")
     self._identifier = self._get_property("identifier", "")
     self._description = self._get_property("description", "")
+
+    self._version = self._get_property("version", 1)
 
   @property
   def date( self ):
@@ -179,41 +189,79 @@ class Bible:
   def description( self ):
     return self._description
 
+  @property
+  def version( self ):
+    """Schema version nubmer"""
+    return self._version
+
   def books( self ):
     """Returns list of all books"""
     self._set_factory( book_factory )
 
-    return self._db.all("SELECT * FROM books")
+    return self._db.all("""SELECT 
+      `books`.`id`,
+      `books`.`osisid`,
+      `books`.`name`,
+      `books`.`title`,
+      `books`.`abbr`
+      FROM books""")
 
   def book( self, book ):
     """Returns single book"""
     self._set_factory( book_factory )
 
-    return self._db.get("SELECT * FROM books WHERE id = ?", (book, ))
+    return self._db.get("""SELECT
+      `books`.`id`,
+      `books`.`osisid`,
+      `books`.`name`,
+      `books`.`title`,
+      `books`.`abbr` FROM books WHERE id = ?""", (book, ))
 
   def chapter( self, book, chapter ):
     """Returns all verses in chapter"""
     self._set_factory( verse_factory )
 
-    return self._db.all("SELECT * FROM verses WHERE book = ? AND chapter = ? ORDER BY verse ASC", (book, chapter))
+    return self._db.all("""SELECT 
+        `verses`.`osisid`,
+        `verses`.`book`,
+        `verses`.`chapter`,
+        `verses`.`verse`,
+        `verses`.`text`,
+        `books`.`name` as book_name
+      FROM verses
+      LEFT JOIN `books` ON `verses`.`book` = `books`.`id`
+      WHERE `verses`.`book` = ? AND `verses`.`chapter` = ?
+      ORDER BY `verses`.`verse` ASC""", (book, chapter))
 
-  def verse( self, book, chapter, index ):
+  def verse( self, book, chapter, verse ):
     """Returns single verse"""
     self._set_factory( verse_factory )
 
-    return self._db.get("SELECT * FROM verses WHERE book = ? AND chapter = ? AND verse = ?", (book, chapter, index))
+    return self._db.get("""SELECT 
+        `verses`.`osisid`,
+        `verses`.`book`,
+        `verses`.`chapter`,
+        `verses`.`verse`,
+        `verses`.`text`,
+        `books`.`name` as book_name
+      FROM verses
+      LEFT JOIN `books` ON `verses`.`book` = `books`.`id` 
+      WHERE `verses`.`book` = ? AND `verses`.`chapter` = ? AND `verses`.`verse` = ?""", 
+      (book, chapter, verse))
 
   def count_verses( self, book, chapter ):
     """Returns number of verses in chapter"""
     self._set_factory()
 
-    return self._db.get("SELECT COUNT(*) as count FROM verses WHERE book = ? AND chapter = ?", (book, chapter))["count"]
+    return self._db.get("SELECT COUNT(*) as count FROM verses WHERE book = ? AND chapter = ?", 
+      (book, chapter))["count"]
 
   def count_chapters( self, book ):
     """Returns number of chapters in book"""
     self._set_factory()
 
-    return self._db.get("SELECT COUNT(*) as count FROM verses WHERE book = ? AND verse = 1", (book, ))["count"]
+    return self._db.get("SELECT COUNT(*) as count FROM verses WHERE book = ? AND verse = 1", 
+      (book, ))["count"]
 
   def match_book( self, keyword ):
     """find book by keyword"""
@@ -221,19 +269,69 @@ class Bible:
 
     keyword = "%" + keyword + "%"
 
-    return self._db.all("""
-            SELECT * FROM books
-            WHERE
-             lowercase(title) LIKE lowercase( ? )
-             OR lowercase(short) LIKE lowercase( ? )
-             OR lowercase(full) LIKE lowercase( ? )
-            """, (keyword, keyword, keyword ))
+    return self._db.all("""SELECT
+      `books`.`id`,
+      `books`.`osisid`,
+      `books`.`name`,
+      `books`.`title`,
+      `books`.`abbr`
+      FROM books
+      WHERE
+       lowercase(title) LIKE lowercase( ? )
+       OR lowercase(short) LIKE lowercase( ? )
+       OR lowercase(full) LIKE lowercase( ? )
+      """, (keyword, keyword, keyword ))
 
   def match_reference( self, keyword ):
     """find verse by keyword"""
     self._set_factory( verse_factory )
 
-    return []
+    # default values
+    chapter = 1
+    verse = 1
+
+    keyword = keyword.lstrip().rstrip().lower()
+
+    # match book
+    match = re.search( r'([0-9]+)$', keyword)
+    # match book and chapter
+    match_chapter = re.search( r'([0-9]+)([\D]{1})([0-9]+)$', keyword )
+    # match book, chapter and verse
+    match_verse = re.search( r'([0-9]+)([\D]{1})([0-9]+)\-([0-9]+)$', keyword )
+
+    if match_verse:
+      chapter = match_verse.group(1)
+      verse = match_verse.group(3)
+      keyword = re.sub( r'([0-9]+)([\D]{1})([0-9]+)\-([0-9]+)$', '', keyword )
+    elif match_chapter:
+      chapter = match_chapter.group(1)
+      verse = match_chapter.group(3)
+      keyword = re.sub( r'([0-9]+)([\D]{1})([0-9]+)$', '', keyword )
+    elif match:
+      chapter = match.group(1)
+      keyword = re.sub( r'([0-9]+)$', '', keyword )
+
+    keyword = "%" + keyword.lstrip().rstrip() + "%"
+    chapter = int(chapter)
+    verse = int(verse)
+
+    return self._db.all("""SELECT
+        `verses`.`osisid`,
+        `verses`.`book`,
+        `verses`.`chapter`,
+        `verses`.`verse`,
+        `verses`.`text`,
+        `books`.`name` as book_name
+      FROM verses
+      LEFT JOIN `books` ON `verses`.`book` = `books`.`id`
+      WHERE
+        (lowercase(`books`.`title`) LIKE ?
+        OR lowercase(`books`.`name`) LIKE ?
+        OR lowercase(`books`.`abbr`) LIKE ?)
+        AND `verses`.`chapter` = ?
+        AND `verses`.`verse` = ?
+      LIMIT 3""",
+      (keyword, keyword, keyword, chapter, verse))
 
   def _set_factory( self, factory = lite.Row ):
     """Set sqlite row factory"""
@@ -243,15 +341,83 @@ class Bible:
     """Get info property"""
     self._set_factory()
 
-    value = self._db.get("SELECT key, value from info WHERE key = ?", (key, ))
+    value = self._db.get("SELECT key, value from properties WHERE key = ?", (key, ))
 
     return value["value"] if value else default
 
   @staticmethod
   def validate( file_path ):
     """Check file to be valid grail bible file format"""
-    return True
+    #return True
+    # if file not exists
+    if not os.path.isfile( file_path ):
+      return False
 
+    # if file doesn't have proper extension
+    if os.path.splitext(file_path)[1] != '.grail-bible':
+      return False
+
+    # check if file have proper structure
+    try:
+      connection = lite.connect( file_path )
+      connection.row_factory = lite.Row
+      c = connection.cursor()
+      flag = True
+
+      c.execute("""SELECT name 
+        FROM sqlite_master 
+        WHERE type='table' AND name in ('books', 'verses', 'properties')
+        ORDER BY name ASC""")
+      tables = c.fetchall()
+
+      if [table['name'] for table in tables] != ['books', 'properties', 'verses']:
+        flag = False
+
+      c.execute("""SELECT 
+          `properties`.`key`,
+          `properties`.`value`,
+          `properties`.`type`
+        FROM properties LIMIT 1""")
+      record = c.fetchone()
+
+      if record.keys() != ['key', 'value', 'type']:
+        flag = False
+
+      c.execute("""SELECT 
+          `books`.`id`,
+          `books`.`osisid`,
+          `books`.`name`,
+          `books`.`title`,
+          `books`.`abbr`
+        FROM books LIMIT 1""")
+      record = c.fetchone()
+
+      if record.keys() != ['id', 'osisid', 'name', 'title', 'abbr']:
+        flag = False
+
+      c.execute("""SELECT 
+          `verses`.`osisid`,
+          `verses`.`book`,
+          `verses`.`chapter`,
+          `verses`.`verse`,
+          `verses`.`text`
+        FROM verses LIMIT 1""")
+      record = c.fetchone()
+
+      if record.keys() != ['osisid', 'book', 'chapter', 'verse', 'text']:
+        flag = False
+
+      c.execute("SELECT key, value from properties WHERE key = ?", ('version', ))
+      
+      # check version
+      if int(c.fetchone()['value']) != 1:
+        flag = False 
+
+      connection.close()
+      
+      return flag
+    except:
+      return False
 
 class BibleHost:
   """Mange bible versions"""
