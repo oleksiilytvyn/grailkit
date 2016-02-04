de77ac747645289b1ab19a708c249f6df11f000b
programer95 <programer95@gmail.com>
2016-02-04 14:46:21 +0200

    fixes

--- grail/sdk/db.py
+++ grail/sdk/db.py
@@ -3,109 +3,113 @@
 
 import os
 import sqlite3 as lite
+from grail.sdk.utils import copy_file
 
+class DataBaseError(Exception):
+    """Base class for DataBase Errors"""
+    pass
 
 class DataBase:
-  """SQLite database wrapper"""
+    """SQLite database wrapper"""
 
-  _connection = None
+    _connection = None
 
-  def __init__( self, file_path, file_copy = False, query = False, create = True ):
-    """Create SQLite database wraper
+    def __init__( self, file_path, file_copy = False, query = False, create = True ):
+        """Create SQLite database wraper
 
-    Args:
-      - file_path: database file path
-      - file_copy: copy file if file_path not exists
-      - query: execute query if file_path not exists
-      - create: create database file or not
-    """
-    directory = os.path.dirname(os.path.realpath( file_path ))
-    execute_query = False
+        Args:
+            - file_path: database file path
+            - file_copy: copy file if file_path not exists
+            - query: execute query if file_path not exists
+            - create: create database file or not
+        """
+        directory = os.path.dirname(os.path.realpath( file_path ))
+        execute_query = False
 
-    if not create and (not os.path.exists( directory ) or
-       not os.path.isfile( file_path )):
-      raise Exception("Database file not exists. Unable to open sqlite file.")
+        if not create and (not os.path.exists( directory ) or
+             not os.path.isfile( file_path )):
+            raise Exception("Database file not exists. Unable to open sqlite file.")
 
-    if not os.path.exists( directory ):
-      os.makedirs( directory )
+        if not os.path.exists( directory ):
+            os.makedirs( directory )
 
-    if not os.path.isfile( file_path ):
-      if file_copy:
-        copy_file( file_copy, file_path )
-      else:
-        open( file_path, 'w+' )
-        execute_query = True
+        if not os.path.isfile( file_path ):
+            if file_copy:
+                copy_file( file_copy, file_path )
+            else:
+                open( file_path, 'w+' )
+                execute_query = True
 
-    self._connection = lite.connect( file_path )
-    self._connection.row_factory = lite.Row
+        self._connection = lite.connect( file_path )
+        self._connection.row_factory = lite.Row
 
-    def lowercase( char ):
-      return char.lower()
+        def lowercase( char ):
+            return char.lower()
 
-    def searchprep( char ):
-      char = re.sub(r'[\[\_\]\.\-\,\!\(\)\"\'\:\;]', '', char)
+        def searchprep( char ):
+            char = re.sub(r'[\[\_\]\.\-\,\!\(\)\"\'\:\;]', '', char)
 
-      return char.lower()
+            return char.lower()
 
-    self._connection.create_function("lowercase", 1, lowercase)
-    self._connection.create_function("searchprep", 1, searchprep)
+        self._connection.create_function("lowercase", 1, lowercase)
+        self._connection.create_function("searchprep", 1, searchprep)
 
-    if execute_query and query:
-      self.cursor.executescript(query)
+        if execute_query and query:
+            self.cursor.executescript(query)
 
-  @property
-  def connection( self ):
-    """Returns sqlite3 connection"""
-    return self._connection
+    @property
+    def connection( self ):
+        """Returns sqlite3 connection"""
+        return self._connection
 
-  @property
-  def cursor( self ):
-    """Returns sqlite3 connection cursor"""
-    return self._connection.cursor()
+    @property
+    def cursor( self ):
+        """Returns sqlite3 connection cursor"""
+        return self._connection.cursor()
 
-  def get( self, query, data=tuple() ):
-    """Execute query and return first record"""
-    cursor = self.cursor
-    cursor.execute(query, data)
+    def get( self, query, data=tuple() ):
+        """Execute query and return first record"""
+        cursor = self.cursor
+        cursor.execute(query, data)
 
-    return cursor.fetchone()
+        return cursor.fetchone()
 
-  def all( self, query, data=tuple() ):
-    """Execute query and return all records"""
-    cursor = self.cursor
-    cursor.execute(query, data)
+    def all( self, query, data=tuple() ):
+        """Execute query and return all records"""
+        cursor = self.cursor
+        cursor.execute(query, data)
 
-    return cursor.fetchall()
+        return cursor.fetchall()
 
-  def close( self ):
-    """Close connection"""
-    self._connection.commit()
-    self._connection.close()
+    def close( self ):
+        """Close connection"""
+        self._connection.commit()
+        self._connection.close()
 
 
 class DataBaseHost:
-  """Host all sqlite database connections"""
+    """Host all sqlite database connections"""
 
-  _list = {}
+    _list = {}
 
-  @staticmethod
-  def get( file_path, file_copy = False, query = False, create = True  ):
-    """Get databse"""
-    
-    file_path = os.path.abspath(file_path)
+    @staticmethod
+    def get( file_path, file_copy = False, query = False, create = True  ):
+        """Get databse"""
+        
+        file_path = os.path.abspath(file_path)
 
-    if file_path in DataBaseHost._list:
-      db = DataBaseHost._list[ file_path ]
-    else:
-      db = DataBase( file_path, file_copy, query, create )
-      DataBaseHost._list[ file_path ] = db
+        if file_path in DataBaseHost._list:
+            db = DataBaseHost._list[ file_path ]
+        else:
+            db = DataBase( file_path, file_copy, query, create )
+            DataBaseHost._list[ file_path ] = db
 
-    return db
+        return db
 
-  @staticmethod
-  def close():
-    """Close all connections"""
-    for key in DataBaseHost._list:
-      DataBaseHost._list[ key ].close()
+    @staticmethod
+    def close():
+        """Close all connections"""
+        for key in DataBaseHost._list:
+            DataBaseHost._list[ key ].close()
 
-    return True
+        return True
