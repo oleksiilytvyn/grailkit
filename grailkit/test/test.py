269b90d2e3b8d1a89a87b6103e7f236ecbe53cdf
Alex Litvin <programer95@gmail.com>
2016-02-17 16:47:33 +0200

    update on db, dna, project + library module

--- grailkit/test/test.py
+++ grailkit/test/test.py
@@ -9,16 +9,15 @@ from grailkit.ui import GApplication
 
 
 class MainWindow(QMainWindow):
-
-    def __init__( self, parent=None ):
+    def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
         self.initUI()
 
-    def initUI( self ):
-        self.setGeometry( 300, 300, 800, 480 )
-        self.setMinimumSize( 320, 240 )
-        self.setWindowTitle( "Grail" )
+    def initUI(self):
+        self.setGeometry(300, 300, 800, 480)
+        self.setMinimumSize(320, 240)
+        self.setWindowTitle("Grail")
         self.show()
 
 
@@ -29,6 +28,7 @@ def main():
     win = MainWindow()
 
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
