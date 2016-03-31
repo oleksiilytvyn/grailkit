523eb616223dbdde8a38f9fa5da184d9486a5fa5
Alex Litvin <programer95@gmail.com>
2016-03-31 22:36:23 +0300

    New dialogs

--- grailkit/ui/__init__.py
+++ grailkit/ui/__init__.py
@@ -2,10 +2,13 @@
 
 # core
 from grailkit.ui.gapplication import GApplication
+
 # widgets
 from grailkit.ui.gwidget import GWidget
+
 # dialogs
 from grailkit.ui.gdialog import GDialog
 from grailkit.ui.gframeless import GFrameless
 from grailkit.ui.gaboutdialog import GAboutDialog
 from grailkit.ui.gmessagedialog import GMessageDialog
+from grailkit.ui.gballoondialog import GBalloonDialog
--- grailkit/ui/gaboutdialog.py
+++ grailkit/ui/gaboutdialog.py
@@ -12,42 +12,74 @@ from grailkit.ui import GDialog, GApplication
 class GAboutDialog(GDialog):
     """Default about dialog"""
 
-    def __init__(self, parent=None, title="", description=""):
+    def __init__(self, parent=None, title="Application", description="version 1.0", icon=None):
+        """A basic about dialog"""
+
         super(GAboutDialog, self).__init__(parent)
+
+        self._title = title
+        self._description = description
+        self._icon = icon
+
+        self.url_report = ""
+        self.url_help = ""
 
         self._init_ui()
 
     def _init_ui(self):
-        self.ui_icon = QLabel(self)
-        self.ui_icon.setPixmap(QPixmap(":/icons/128.png"))
-        self.ui_icon.setAlignment(Qt.AlignCenter)
-        self.ui_icon.setStyleSheet("background: black;")
-        self.ui_icon.setGeometry(55, 68, 48, 48)
 
-        self.ui_title = QLabel("Grail", self)
-        self.ui_title.setGeometry(160, 34, 311, 26)
+        self._ui_pixmap = QApplication.style().standardIcon(QStyle.SP_MessageBoxInformation)
 
-        self.ui_description = QPlainTextEdit("description", self)
-        self.ui_description.setReadOnly(True)
-        self.ui_description.setGeometry(160, 74, 311, 100)
+        self._ui_icon = QLabel(self)
+        self._ui_icon.setPixmap(self._ui_pixmap.pixmap(64))
+        self._ui_icon.setAlignment(Qt.AlignCenter)
+        self._ui_icon.setGeometry(48, 52, 64, 64)
 
-        self.ui_btn_help = QPushButton("Help")
-        self.ui_btn_report = QPushButton("Report a problem")
+        self._ui_title = QLabel(self._title, self)
+        self._ui_title.setStyleSheet("font-size: 20pt;")
+        self._ui_title.setGeometry(160, 34, 311, 26)
+
+        self._ui_description = QPlainTextEdit(self._description, self)
+        self._ui_description.setStyleSheet("background: transparent;")
+        self._ui_description.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self._ui_description.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self._ui_description.setReadOnly(True)
+        self._ui_description.setGeometry(156, 74, 311, 88)
+
+        self._ui_btn_help = QPushButton("Help")
+        self._ui_btn_help.clicked.connect(self.help)
+
+        self._ui_btn_report = QPushButton("Report a problem")
+        self._ui_btn_report.clicked.connect(self.report)
+
         self.ui_btn_close = QPushButton("Close")
+        self.ui_btn_close.setDefault(True)
+        self.ui_btn_close.clicked.connect(self.close)
 
-        self.ui_buttons_layout = QHBoxLayout()
-        self.ui_buttons_layout.addWidget(self.ui_btn_help)
-        self.ui_buttons_layout.addWidget(self.ui_btn_report)
-        self.ui_buttons_layout.addWidget(self.ui_btn_close)
+        self._ui_buttons_layout = QBoxLayout(QBoxLayout.LeftToRight)
+        self._ui_buttons_layout.addWidget(self._ui_btn_help)
+        self._ui_buttons_layout.addStretch()
+        self._ui_buttons_layout.addWidget(self._ui_btn_report)
+        self._ui_buttons_layout.addWidget(self.ui_btn_close)
 
-        self.ui_buttons = QWidget(self)
-        self.ui_buttons.setLayout(self.ui_buttons_layout)
-        self.ui_buttons.setGeometry(16, 187, 455, 40)
+        self._ui_buttons = QWidget(self)
+        self._ui_buttons.setLayout(self._ui_buttons_layout)
+        self._ui_buttons.setGeometry(8, 172, 484 - 16, 50)
 
-        self.setWindowTitle('About')
+        self.setWindowTitle("About %s" % (self._title,))
         self.setWindowFlags(Qt.WindowCloseButtonHint)
         self.setGeometry(100, 100, 484, 224)
         self.setFixedSize(484, 224)
+
+    def help(self):
+        """Open a web page"""
+        url = QUrl(self.url_help)
+        QDesktopServices.openUrl(url)
+
+    def report(self):
+        """Open a web page"""
+        url = QUrl(self.url_report)
+        QDesktopServices.openUrl(url)
 
 # test a dialog
 if __name__ == '__main__':
