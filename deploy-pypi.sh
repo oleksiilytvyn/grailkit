cf801476b4e3aa38f324be697fb0d9c74a411584
Alex Litvin <programer95@gmail.com>
2017-12-07 22:12:22 +0200

    PyPi pipelines integration

---
+++ deploy-pypi.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+if [[ "$PYPI_USERNAME" || -z "$PYPI_PASSWORD" ]]; then
+    echo "You must set PYPI_USERNAME and PYPI_PASSWORD to run this script"
+    exit 1
+fi
+
+cat << EOF > /root/.pypirc
+[distutils]
+index-servers =
+  pypi
+
+[pypi]
+repository=https://upload.pypi.org/legacy/
+username=${PYPI_USERNAME}
+password=${PYPI_PASSWORD}
+EOF
+
+python setup.py sdist upload -r pypi
