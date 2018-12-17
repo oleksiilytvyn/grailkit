cb93e1ad7197f37f909b7cfa200cf3f60683fb26
Alex Litvin <programer95@gmail.com>
2018-12-17 19:46:17 +0200

    Little cleanup

--- grailkit/__init__.py
+++ grailkit/__init__.py
@@ -15,7 +15,7 @@ import os
 from grailkit.util import data_location
 
 # library version
-__version__ = '0.6.9'
+__version__ = '0.7'
 
 # path to shader folder
 PATH_SHARED = data_location("grail-shared")
